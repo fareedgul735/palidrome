2e6ad0f3161d95558d4160f0df6d5d72b7119b2d
fareedgul <fareedgul735@gmail.com>
2025-09-09 03:53:24 -0700

    fix/changes

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const palindromeBtn = document.getElementById("covertText");
 const palindromeField = document.getElementById("palindromeField");
 const outputPalindrome = document.getElementById("outputPalindrome");
 const err = document.getElementById("error");
+
 const foo = (palindrome) => {
-  const palindromeConverted = palindrome.split("").reverse().join("");
-  return palindromeConverted;
+  return palindrome.split("").reverse().join("");
 };
 
-const functionCoverted = () => {
+const palindromeText = () => {
   const palinVal = palindromeField.value.trim();
   if (!palinVal) {
     err.innerText = "field are required!";
     palindromeField.style.border = "1px solid red";
-  } else {
-    outputPalindrome.innerText = foo(palinVal);
-    palindromeField.value = "";
-    err.innerText = "";
-    palindromeField.style.border = "1px solid gray";
+    return;
   }
+
+  outputPalindrome.innerText = foo(palinVal);
+  palindromeField.value = "";
+  err.innerText = "";
+  palindromeField.style.border = "1px solid gray";
 };
-palindromeBtn.addEventListener("click", functionCoverted);
+palindromeBtn.addEventListener("click", palindromeText);
